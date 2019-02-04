46de4fb9489149cb55ab43dfb3a56b6d4213a8bf
Brian Vo <ovnairb@gmail.com>
2019-02-03 17:50:14 -0800

    added chi router and new test getName route

--- server/server.go
+++ server/server.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 )
 
 func main() {
+	port, exists := os.LookupEnv("PORT")
+
+	if !exists {
+		port = "8080"
+	}
+	log.Printf("Starting server on port %s\n", port)
+
 	router := chi.NewRouter()
-	router.Get("/", func(w http.ResponseWriter, router *http.Request) {
-		w.Write([]byte("welcome"))
+	router.Get("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("hi"))
 	})
-	http.ListenAndServe(":8080", router)
+	router.Route("/test", func(router chi.Router) {
+		router.Get("/{name}", getName)
+	})
+	http.ListenAndServe(":"+port, router)
+
+}
+
+func getName(w http.ResponseWriter, req *http.Request) {
+	//chi.UrlParam to read http url parameter
+	name := chi.URLParam(req, "name")
+	//string print
+	w.Write([]byte(fmt.Sprintf("My name is %s", name)))
 }
