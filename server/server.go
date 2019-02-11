e3cccf0c57329d25adc21b30fbe659b4d67a9278
Brian Vo <bryevo@gmail.com>
2019-02-11 00:05:26 -0800

    update test file upload url

--- server/server.go
+++ server/server.go
@@ -186,7 +186,7 @@ var uploadTest = []byte(`
        <title>Upload file</title>
 </head>
 <body>
-<form enctype="multipart/form-data" action="http://localhost:8080/upload/send" method="post">
+<form enctype="multipart/form-data" action="https://doggy-date-go.herokuapp.com/upload/send" method="post">
     <input type="file" name="uploadfile" />
     <input type="hidden" name="token" value="{{.}}"/>
     <input type="submit" value="upload" />
